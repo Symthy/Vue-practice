0113956a4484df76b4f1003ccb6e5d15c50dce25
Satoshi Yamashita <sympathia3104@gmail.com>
2020-11-30 21:32:43 +0900

    feat: first commit

---
+++ todosample/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <TodoApplication></TodoApplication>
+  </div>
+</template>
+
+<script>
+import TodoApplication from './components/TodoApplication.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoApplication
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todosample/src/components/TodoApplication.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" v-model="message" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoApplication",
+  data() {
+    return {
+      message: "Todo Application Sample"
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
